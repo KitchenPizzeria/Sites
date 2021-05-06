6d6b71b8a1fedf09596143b38f1cd3490c77467d
KitchenPizzeria <70120980+KitchenPizzeria@users.noreply.github.com>
2021-05-07 00:14:39 +0100

    Initial Commit

---
+++ Site-Seeing/Side Menu Views/SideMenuView.swift
@@ -0,0 +1,39 @@
+//
+//  SideMenuView.swift
+//  Site-Seeing
+//
+//  Created by Joseph Meyrick on 06/05/2021.
+//
+
+import SwiftUI
+
+struct SideMenuView: View  {
+    
+    @Binding var isShowing: Bool
+
+    var body: some View {
+        
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color.blue, Color.purple]), startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            
+            VStack {
+                
+                SideMenuHeaderView(isShowing: $isShowing)
+                    .frame(height: 240)
+                
+                ForEach(0..<5) { _ in
+                    SideMenuOptionView()
+                }
+                
+                Spacer()
+                
+            }
+            
+            Spacer()
+            
+        }
+        .navigationBarHidden(true)
+        
+    }
+}
---
+++ Site-Seeing/Site_SeeingApp.swift
@@ -0,0 +1,18 @@
+//
+//  Site_SeeingApp.swift
+//  Site-Seeing
+//
+//  Created by Joseph Meyrick on 04/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct Site_SeeingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
